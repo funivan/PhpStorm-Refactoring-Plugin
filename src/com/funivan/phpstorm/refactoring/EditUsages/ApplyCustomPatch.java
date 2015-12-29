6adf09d92460144177bba12b53f20ad465e0fd55
Ivan Scherbak <dev@funivan.com>
2015-12-29 14:02:16 +0200

    close: #6

--- src/com/funivan/phpstorm/refactoring/EditUsages/ApplyCustomPatch.java
+++ src/com/funivan/phpstorm/refactoring/EditUsages/ApplyCustomPatch.java
@@ -12,19 +12,23 @@ import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.PsiDocumentManager;
+import com.intellij.ui.awt.RelativePoint;
 
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @todo Add notification when action complete
+ * @author Ivan Scherbak <dev@funivan.com>
  * @todo FeatureUsageTracker.getInstance().triggerFeatureUsed("scratch"); add triggers for patch usages
- *
- * @author  Ivan Scherbak <dev@funivan.com>
  */
 public class ApplyCustomPatch extends AnAction {
 
@@ -102,6 +106,8 @@ public class ApplyCustomPatch extends AnAction {
                 ApplicationManager.getApplication().runWriteAction(new Runnable() {
                     @Override
                     public void run() {
+                        int items = 0;
+                        int files = 0;
                         for (DocumentReplaces doc : documentsForChange.values()) {
 
                             List<ReplaceStructure> replaces = doc.getReplaces();
@@ -112,8 +118,22 @@ public class ApplyCustomPatch extends AnAction {
 
                             for (ReplaceStructure r : replaces) {
                                 doc.getDocument().replaceString(r.getStart(), r.getEnd(), r.getValue());
+                                items++;
                             }
+                            files++;
                         }
+
+                        StatusBar statusBar = WindowManager.getInstance().getStatusBar(project);
+                        if (statusBar != null) {
+                            JBPopupFactory.getInstance()
+                                    .createHtmlTextBalloonBuilder("Processed. Files: " + files + " lines:" + items, MessageType.INFO, null)
+                                    .setFadeoutTime(7500)
+                                    .createBalloon()
+                                    .show(RelativePoint.getCenterOf(statusBar.getComponent()), Balloon.Position.atRight);
+
+                        }
+
+
                     }
                 });
             }
